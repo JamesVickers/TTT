554e1494772f17e357d8ce29b44740eff696d543
James Vickers <jvickersdesign@gmail.com>
2019-05-11 10:36:00 +0100

    End of old design

---
+++ public/app.js
@@ -0,0 +1,12 @@
+//alert("hello");
+function showModal() {
+    $(".mobile-nav-modal")
+      .removeClass("modal-hide")
+      .addClass("modal-show");
+  
+    $("body").css("overflow", "hidden");
+  }
+  
+  $("#openMenu").on("click", function() {
+    showModal();
+  });
