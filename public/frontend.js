7167125a5175bf3827db296cb8937158266063fe
James Vickers <jvickersdesign@gmail.com>
2019-07-13 10:48:40 +0100

    index hex patterns replaced, now fixed on all devices and orientations

--- public/frontend.js
+++ public/frontend.js
@@ -23,11 +23,15 @@ $("#closeMenu").on("click", function() {
   hideModal();
 });
 
+
+
 //hide modal automatically if window is resized > 700px
 $(window).resize(function() {
   if (window.matchMedia("(min-width: 700px)").matches) {
     hideModal();
   }
+  //let vh = window.innerHeight * 0.01;
+  //document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
 ///change work button images from greyscale to color on hover
