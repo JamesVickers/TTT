6158973664e0cbad63b60109dc6f587729db86f7
James Vickers <jvickersdesign@gmail.com>
2019-08-31 14:22:55 +0100

    Privacy policy added and cookie banner edited

--- public/cookie.js
+++ public/cookie.js
@@ -2,19 +2,25 @@ var config = {
   //test key and product type - change before going live
   // apiKey: "592b99ebdf88c091dad9b556b6d8de236ac97687",
   // product: "PRO_MULTISITE",
+
   //production key linked to treetops.tech
   apiKey: "4153d1360fba91f35447d9683780224125883891",
   //production product type
   product: "COMMUNITY",
 
+  text: {
+    title: "This site uses cookies",
+    intro: "Some of these cookies are essential, while others help us to improve your experience."
+  },
+
   optionalCookies: [
     {
       name: "analytics",
       label: "Analytics",
-      description:
-        "Analytical cookies help us to improve our website by collecting and reporting information on its usage.",
+      description: "Analytical cookies help us to improve our website by collecting and reporting information on its usage.",
       cookies: ["_ga", "_gid", "_gat"],
       logConsent: false,
+      recommendedState: true,
       onAccept: function() {
         window.dataLayer = window.dataLayer || [];
         function gtag() {
@@ -31,6 +37,7 @@ var config = {
         // End Google Analytics
       }
     }
+
     /*,{
       name: "marketing",
       label: "Marketing",
@@ -51,8 +58,15 @@ var config = {
     }*/
   ],
 
+  statement: {
+    description: "For more information visit our ",
+    name: "Privacy Statement",
+    url: "https://treetops.tech/privacy",
+    updated: "31/08/2019"
+  },
+
   position: "LEFT",
-  theme: "DARK"
+  theme: "LIGHT"
 };
 
 CookieControl.load(config);
